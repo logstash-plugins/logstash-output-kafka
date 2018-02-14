5bad1790fb12746a7bd3adf201f1928774558ea0
Rob Bavey <rob.bavey@elastic.co>
2018-02-14 10:52:33 -0500

    Test: DO NOT COMMIT
    Investigate why travis builds are hanging

--- kafka_test_teardown.sh
+++ kafka_test_teardown.sh
@@ -7,3 +7,8 @@ echo "Stopping Kafka broker"
 kafka/bin/kafka-server-stop.sh
 echo "Stopping zookeeper"
 kafka/bin/zookeeper-server-stop.sh
+echo "killing everything"
+sleep 30s
+ps aux
+kill $(jobs -p)
+ps aux
