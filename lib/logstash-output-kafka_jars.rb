183f6c3c6d46632f288037661abaa75c8cbf017f
Joao Duarte <jsvduarte@gmail.com>
2018-05-18 15:57:14 +0100

    tighten jar dependencies

--- lib/logstash-output-kafka_jars.rb
+++ lib/logstash-output-kafka_jars.rb
@@ -2,8 +2,6 @@
 
 require 'jar_dependencies'
 require_jar('org.apache.kafka', 'kafka-clients', '1.1.0')
-require_jar('org.slf4j', 'slf4j-log4j12', '1.7.25')
+require_jar('org.slf4j', 'slf4j-api', '1.7.25')
 require_jar('org.lz4', 'lz4-java', '1.4')
 require_jar('org.xerial.snappy', 'snappy-java', '1.1.7.1')
-require_jar('org.slf4j', 'slf4j-api', '1.7.25')
-require_jar('log4j', 'log4j', '1.2.17')
