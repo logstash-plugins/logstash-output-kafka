429564a94b1a96d4be83d292d60016bfc48045c5
Rob Bavey <rob.bavey@elastic.co>
2019-07-09 14:14:08 -0400

    Bump Kafka download version (#225)
    Bump kafka download version to fix broken tests

--- kafka_test_setup.sh
+++ kafka_test_setup.sh
@@ -5,7 +5,7 @@ set -ex
 if [ -n "${KAFKA_VERSION+1}" ]; then
   echo "KAFKA_VERSION is $KAFKA_VERSION"
 else
-   KAFKA_VERSION=2.0.1
+   KAFKA_VERSION=2.1.1
 fi
 
 echo "Downloading Kafka version $KAFKA_VERSION"
